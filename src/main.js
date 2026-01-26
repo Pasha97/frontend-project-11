ef94d86cb407a265d5b35e3371c915e1e3c5d6f5
pavelignatov <97_ignatov.pavel@mail.ru>
2026-01-26 20:05:52 +0300

    Настройка окружения

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import 'bootstrap'
+
+document.querySelector('#app').innerHTML = `
+    <a href="https://vite.dev" target="_blank">
+        Trst
+    </a>
+`
+
+// setupCounter(document.querySelector('#counter'))
